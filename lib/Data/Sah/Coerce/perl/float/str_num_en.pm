f26dee3710a26960c331fac4446b852086e8ff8d
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-06-11 23:11:01 +0700

    Release v0.001

--- lib/Data/Sah/Coerce/perl/float/str_num_en.pm
+++ lib/Data/Sah/Coerce/perl/float/str_num_en.pm
@@ -10,4 +10,4 @@ use alias::module 'Data::Sah::Coerce::perl::num::str_num_en';
 
 The rule is not enabled by default. You can enable it in a schema using e.g.:
 
- ["int", "x.perl.coerce_rules"=>["str_num_en"]]
+ ["float", "x.perl.coerce_rules"=>["str_num_en"]]
